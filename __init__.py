1cfd7072efc1be6628a5085cc8353c01cb0dd8a7
ventus <ventuss550@gmail.com>
2024-11-27 09:42:52 +0100

    refactor

---
+++ __init__.py
@@ -0,0 +1,6 @@
+from . import data_utils
+from . import paths
+# from .src import vae_pipeline
+from . import visualize
+
+__all__ = [data_utils, paths, visualize]
--- paths.py
+++ paths.py
@@ -1,6 +1,6 @@
 import os
 
-ROOT_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
+ROOT_DIR = os.path.dirname(os.path.abspath(__file__))
 
 DATASETS_DIR = os.path.join(ROOT_DIR, "data")
 
